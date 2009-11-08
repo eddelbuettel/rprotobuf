dee9c45aa56ffe4d0cfc620c4b07bba744bdeabb
Romain François <romain@r-enthusiasts.com>
2009-11-08 19:28:12 +0000

    minor doc tweaks

--- src/RconnectionCopyingInputStream.cpp
+++ src/RconnectionCopyingInputStream.cpp
@@ -7,21 +7,26 @@ namespace rprotobuf{
 		connection_id = id ;
 	}
 	
+	/** 
+	 * call readBin to read size bytes from R
+	 * @param buffer buffer to fill with at most size bytes
+	 * @param size maximum number of bytes
+	 *
+	 */
 	int	RconnectionCopyingInputStream::Read(void * buffer, int size){
 		
-		/* call readBin to read size bytes from R */
-		
+		/* FIXME: maybe we should cache these 4 objects instead of recalculating them all the time */
 		SEXP con = PROTECT( Rf_ScalarInteger(connection_id) );
 		SEXP what = PROTECT( Rf_allocVector(RAWSXP, 0) ) ;
 		SEXP n = PROTECT( Rf_ScalarInteger( size ) );
 		SEXP call = PROTECT( Rf_lang4( Rf_install( "readBin" ), con, what, n) ) ; 
+		
 		SEXP res = PROTECT( Rf_eval( call, R_GlobalEnv ) ); 
 		
 		int len = LENGTH( res ) ;
 		memcpy( buffer, RAW(res), len ) ;
 		
 		UNPROTECT( 5 ) ; /* res, call, n, what, con */ 
-		
 		return len ;
 	}
 }
