3701d969273d605eb82cf6e9c88bff0d7f1440cf
Romain François <romain@r-enthusiasts.com>
2009-11-21 08:52:30 +0000

    open the connection at start on read

--- R/read.R
+++ R/read.R
@@ -9,11 +9,20 @@ function(descriptor, input ){
 	.Call( "readMessageFromFile", descriptor@pointer, file, PACKAGE = "RProtoBuf" ) 
 } )
 
+# setMethod( "read", c( descriptor = "protobufDescriptor", input = "raw" ), function(descriptor, input ){
+# 	# .Call( "readMessageFromRawVector", descriptor@pointer, input, PACKAGE="RProtoBuf" )
+# } )
+
 setMethod( "read", c( descriptor = "protobufDescriptor" ), 
 function( descriptor, input ){
 	if( !inherits( input, "connection" ) ){ 
 		stop( "can only read from connections" )
 	}
-	.Call( "readMessageFromConnection", descriptor@pointer, input, PACKAGE = "RProtoBuf" )
+	sc <- summary( input )
+	wasopen <- identical( sc[["opened"]], "opened" )
+	if( !wasopen ) open( input )
+	message <- .Call( "readMessageFromConnection", descriptor@pointer, input, PACKAGE = "RProtoBuf" )
+	if( !wasopen ) close( input )
+	message
 } )
 
