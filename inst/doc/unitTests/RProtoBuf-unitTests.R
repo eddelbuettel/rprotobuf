c089d95fcddb7d43276741d60c8042d36d5e6eb3
Romain François <romain@r-enthusiasts.com>
2010-02-01 14:56:26 +0000

    OS_type: unix (for now)

--- inst/doc/unitTests/RProtoBuf-unitTests.R
+++ inst/doc/unitTests/RProtoBuf-unitTests.R
@@ -3,11 +3,11 @@ pkg <- "RProtoBuf"
 path <- system.file("unitTests", package = pkg)
 testSuite <- defineTestSuite(name=paste(pkg, "unit testing"), dirs = path)
 tests <- runTestSuite(testSuite)
-printHTMLProtocol(tests, fileName= sprintf( "%s-unitTests.html" , pkg ) )
-printTextProtocol(tests, fileName= sprintf( "%s-unitTests.txt"  , pkg ) )
+printHTMLProtocol(tests, fileName= sprintf( "unitTests-results/%s-unitTests.html" , pkg ) )
+printTextProtocol(tests, fileName= sprintf( "unitTests-results/%s-unitTests.txt"  , pkg ) )
 if( file.exists( "/tmp" ) ){
-	file.copy( sprintf( "%s-unitTests.txt" , pkg ) , "/tmp", overwrite = TRUE )
-	file.copy( sprintf( "%s-unitTests.html", pkg ) , "/tmp", overwrite = TRUE )
+	file.copy( sprintf( "unitTests-results/%s-unitTests.txt" , pkg ) , "/tmp", overwrite = TRUE )
+	file.copy( sprintf( "unitTests-results/%s-unitTests.html", pkg ) , "/tmp", overwrite = TRUE )
 }
 Sweave( sprintf('%s-unitTests.Rnw', pkg) )
 texi2dvi( sprintf( '%s-unitTests.tex', pkg) , pdf = TRUE, clean = TRUE )
