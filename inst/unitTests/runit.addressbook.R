c4f829956fb26ae730481abe42789c3693e7f1d4
Dirk Eddelbuettel <edd@debian.org>
2010-10-13 20:10:45 +0000

    fixed the unit tests, albeit in a very inelegant way

--- inst/unitTests/runit.addressbook.R
+++ inst/unitTests/runit.addressbook.R
@@ -3,21 +3,25 @@
 ##   demo(addressbook)  ## creates 'book'
 ##   serialize(book, "/tmp/addressbook.pb")
 
-# this is executed before each test function
+# this is executed before each test function -- but somehow does not work
 .setUp <- function() {
-    #addressbook.proto.file <- system.file( "proto", "addressbook.proto", package = "RProtoBuf" )
-    #readProtoFiles( file = addressbook.proto.file )
-    file <- system.file( "examples", "addressbook.pb", package = "RProtoBuf" )
-    book <<- read( tutorial.AddressBook, file )
+    #file <- system.file( "examples", "addressbook.pb", package = "RProtoBuf" )
+    #book <- read( tutorial.AddressBook, file )
 }
 
 test.size <- function() {
+    # re-set file and book
+    file <- system.file( "examples", "addressbook.pb", package = "RProtoBuf" )
+    book <- read( tutorial.AddressBook, file )
     checkEquals(book$size("person"),        2,   msg="Number of persons")
     checkEquals(bytesize(book),             125, msg="Bytes in book")
     checkEquals(bytesize(book$person[[1]]), 60,  msg="Bytes of first person message")
 }
 
 test.personOne <- function() {
+    # re-set file and book
+    file <- system.file( "examples", "addressbook.pb", package = "RProtoBuf" )
+    book <- read( tutorial.AddressBook, file )
     checkEquals(book$person[[1]]$name,              "Romain Francois",   msg="First person name")
     checkEquals(book$person[[2]]$name,              "Dirk Eddelbuettel", msg="First person name")
     checkEquals(book$person[[1]]$id,                1,   		 msg="First person id")
