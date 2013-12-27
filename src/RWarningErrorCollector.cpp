8b24355a6010c7118a4483bbfa3e7474f2b44b15
Murray Stokely <murray@stokely.org>
2013-12-27 23:48:19 +0000

    whitespace change only: clang-format

--- src/RWarningErrorCollector.cpp
+++ src/RWarningErrorCollector.cpp
@@ -1,13 +1,13 @@
 #include "rprotobuf.h"
-#include "RWarningErrorCollector.h" 
+#include "RWarningErrorCollector.h"
 
 namespace rprotobuf {
 
-	void RWarningErrorCollector::AddError(const std::string& filename, int line, int column,
-		const std::string& message) {
-		
-    	Rprintf( "%s:%d:%d:%s\n", filename.c_str(), line+1, column+1, message.c_str() ) ;
-	}
+void RWarningErrorCollector::AddError(const std::string& filename, int line,
+                                      int column, const std::string& message) {
 
-} // namespace rprotobuf
+    Rprintf("%s:%d:%d:%s\n", filename.c_str(), line + 1, column + 1,
+            message.c_str());
+}
 
+}  // namespace rprotobuf
--- src/RconnectionCopyingInputStream.cpp
+++ src/RconnectionCopyingInputStream.cpp
@@ -1,35 +1,35 @@
 #include "rprotobuf.h"
 #include "RconnectionCopyingInputStream.h"
 
-namespace rprotobuf{
-	/* N.B. connection must be opened in binary mode due to call
-	 * to readBin below. */
-	RconnectionCopyingInputStream::RconnectionCopyingInputStream(int id) : 
-                connection_id(id),
-                failure(false) {}
-	
-	/** 
-	 * call readBin to read size bytes from R
-	 * 
-	 * @param buffer buffer to fill with at most size bytes
-	 * @param size maximum number of bytes
-	 *
-	 * @return the number of bytes actually read
-	 */
-	int	RconnectionCopyingInputStream::Read(void * buffer, int size){
-		Rcpp::Language call( "readBin", connection_id, Rcpp::RawVector(0), size ) ;
-		Rcpp::RawVector res ;
-		try{
-			res = call.eval();
-		}  catch( ... ){
-			/* Failed to read anything from the connection,
-			 * could have been permissions, or connection opened
-			 * in the wrong type, etc. */
-                        failure = true;
-			return -1 ;
-		}
-		int len = res.size() ;
-		memcpy( buffer, reinterpret_cast<const void*>(res.begin()), len) ;
-		return len ;
-	}
+namespace rprotobuf {
+/* N.B. connection must be opened in binary mode due to call
+ * to readBin below. */
+RconnectionCopyingInputStream::RconnectionCopyingInputStream(int id)
+    : connection_id(id), failure(false) {}
+
+/**
+ * call readBin to read size bytes from R
+ *
+ * @param buffer buffer to fill with at most size bytes
+ * @param size maximum number of bytes
+ *
+ * @return the number of bytes actually read
+ */
+int RconnectionCopyingInputStream::Read(void* buffer, int size) {
+    Rcpp::Language call("readBin", connection_id, Rcpp::RawVector(0), size);
+    Rcpp::RawVector res;
+    try {
+        res = call.eval();
+    }
+    catch (...) {
+        /* Failed to read anything from the connection,
+         * could have been permissions, or connection opened
+         * in the wrong type, etc. */
+        failure = true;
+        return -1;
+    }
+    int len = res.size();
+    memcpy(buffer, reinterpret_cast<const void*>(res.begin()), len);
+    return len;
+}
 }
