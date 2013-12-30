4dd3c401b9fc5d4922c3addc5bf50a3e703d2b82
Murray Stokely <murray@stokely.org>
2013-12-30 21:04:24 +0000

    Whitespace change only (clang-format). * 'namespace{' -> 'namespace {' * No indentation from namespace level. * 4 space indentation level.
    No diff visible with svn diff -x -w

--- src/ConnectionCopyingOutputStream.h
+++ src/ConnectionCopyingOutputStream.h
@@ -1,26 +1,26 @@
 #ifndef RPROTOBUF_ConnectionCopyingOutputStream_H
 #define RPROTOBUF_ConnectionCopyingOutputStream_H
 
-namespace rprotobuf{
+namespace rprotobuf {
 
-	class ConnectionCopyingOutputStream : public GPB::io::CopyingOutputStream {
-		public:	
-			ConnectionCopyingOutputStream( SEXP con );
-			bool Write(const void * buffer, int size) ;
-	
-		private: 
-			/* 
-			 	the actual connection object from R
-				it is protected by the external pointer that 
-				wraps the ConnectionInputStream, which is the only 
-				thing that uses this class, so we don't need to take
-				care of GC here
-			*/
-			SEXP con ;
-			
-			Rcpp::Function writeBin ;
-} ;
+class ConnectionCopyingOutputStream : public GPB::io::CopyingOutputStream {
+   public:
+    ConnectionCopyingOutputStream(SEXP con);
+    bool Write(const void* buffer, int size);
 
-} // namespace rprotobuf
+   private:
+    /*
+            the actual connection object from R
+            it is protected by the external pointer that
+            wraps the ConnectionInputStream, which is the only
+            thing that uses this class, so we don't need to take
+            care of GC here
+    */
+    SEXP con;
+
+    Rcpp::Function writeBin;
+};
+
+}  // namespace rprotobuf
 
 #endif
--- src/RconnectionCopyingInputStream.h
+++ src/RconnectionCopyingInputStream.h
@@ -1,20 +1,20 @@
 #ifndef RPROTOBUF_RconnectionCopyingInputStream_H
 #define RPROTOBUF_RconnectionCopyingInputStream_H
 
-namespace rprotobuf{
+namespace rprotobuf {
 
-	class RconnectionCopyingInputStream : public GPB::io::CopyingInputStream {
-		public:	
-			RconnectionCopyingInputStream( int id );
-	
-			int Read(void * buffer, int size) ;
-			bool Failure() { return(failure); }
-	
-		private: 
-			int connection_id ;
-			bool failure;
-} ;
+class RconnectionCopyingInputStream : public GPB::io::CopyingInputStream {
+   public:
+    RconnectionCopyingInputStream(int id);
 
-} // namespace rprotobuf
+    int Read(void* buffer, int size);
+    bool Failure() { return (failure); }
+
+   private:
+    int connection_id;
+    bool failure;
+};
+
+}  // namespace rprotobuf
 
 #endif
