bbaad699c13790e95ad5b7cfc4f23898f3f9b25c
Murray Stokely <murray@stokely.org>
2013-12-30 21:49:04 +0000

    Add comments to the docuementation for directory level variables and also the FoldingMode.
    Also, turn on show-trailing-whitespace so that errant whitespace at
    end of line is highlighted so it can be seen/removed before checkin.

--- src/.dir-locals.el
+++ src/.dir-locals.el
@@ -1,6 +1,14 @@
+; Emacs customization settings to apply to every file in this directory.
+; See http://www.emacswiki.org/emacs/DirectoryVariables
+;
+; Note some of the files include '{{{' and '}}}' annotations to be used
+; with Folding Mode: http://www.emacswiki.org/emacs/FoldingMode
 ((nil . ((indent-tabs-mode . nil)
-	 (tab-width . 4)))
+         (tab-width . 4)
+         (show-trailing-whitespace . t)))
  (c-mode . ((c-indent-level . 4)
-	    (c-basic-offset . 4)))
+            (c-basic-offset . 4)
+            (show-trailing-whitespace . t)))
  (c++-mode . ((c-indent-level . 4)
-	      (c-basic-offset . 4))))
+              (c-basic-offset . 4)
+              (show-trailing-whitespace . t))))
