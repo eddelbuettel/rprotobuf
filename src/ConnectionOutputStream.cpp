f56b356b56b6fe61834ff74514fd5a50d11aa6c0
Murray Stokely <murray@stokely.org>
2013-12-31 20:09:12 +0000

    Explicitly cast a function return value to void to make it clearer we are ignoring it.
    Suggested by: Flexelint

--- src/ConnectionOutputStream.cpp
+++ src/ConnectionOutputStream.cpp
@@ -16,12 +16,11 @@ ConnectionOutputStream::~ConnectionOutputStream() {
     if (!was_open) {
         /* then we need to close it */
         SEXP call = PROTECT(Rf_lang2(Rf_install("close"), con));
-        Rf_eval(call, R_GlobalEnv);
+        (void)Rf_eval(call, R_GlobalEnv); // Ignore SEXP return val of close().
         UNPROTECT(1); /* call */
     }
-    /* con will be disposed by the R GC, it is
-       protected as part of the protection of the
-       external pointer that wraps this */
+    /* con will be disposed by the R GC, it is protected as part of
+       the protection of the external pointer that wraps this */
 }
 
 }  // namespace rprotobuf
