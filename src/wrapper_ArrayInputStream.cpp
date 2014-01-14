c5bb5acffc04943619de6ba3df1df83f7bbbd0a0
Murray Stokely <murray@stokely.org>
2014-01-14 06:20:49 +0000

    Oops, inadvertant debugging change submitted.  Revert.

--- src/wrapper_ArrayInputStream.cpp
+++ src/wrapper_ArrayInputStream.cpp
@@ -2,12 +2,11 @@
 
 #include "rprotobuf.h"
 #include "RcppMacros.h"
-#include <stdio.h>
+
 namespace rprotobuf {
 
 RPB_FUNCTION_2(S4_ArrayInputStream, ArrayInputStream__new, Rcpp::RawVector payload,
                int block_size) {
-    printf("In ArrayInputStream__new, returning S4_ArrayInputStream(payload, blocksize)\n");
     return S4_ArrayInputStream(payload, block_size);
 }
 }
