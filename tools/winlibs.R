2ae8a50662dfad6248d474e1b8bc6a06a87d0cb0
Jeroen Ooms <jeroenooms@gmail.com>
2015-07-08 16:00:50 +0200

    update win lib url

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,7 +2,7 @@
 # by Jeroen Ooms. Last updated: December 2014.
 if(!file.exists("../windows/protobuf-2.6.1/include/google/protobuf/descriptor.h")){
   setInternet2()
-  download.file("http://eddelbuettel.github.io/rprotobuf/windows/protobuf-2.6.1.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/libprotobuf/archive/v2.6.1.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
