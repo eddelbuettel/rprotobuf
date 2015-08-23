c0298716e98969cf9cf891812d57ebbf4591ebaa
Jeroen Ooms <jeroenooms@gmail.com>
2015-08-23 20:35:54 +0200

    fix for R 3.3 on Windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against libprotobuf libraries that were compiled with the Rtools gcc toolchain
 # by Jeroen Ooms. Last updated: December 2014.
 if(!file.exists("../windows/protobuf-2.6.1/include/google/protobuf/descriptor.h")){
-  setInternet2()
+  if(getRversion() <= "3.2.2") setInternet2()
   download.file("https://github.com/rwinlib/protobuf/archive/v2.6.1.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
