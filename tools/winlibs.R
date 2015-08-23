377286c8de41e40588c1024958842ac1b1ee0623
Dirk Eddelbuettel <edd@debian.org>
2015-08-23 13:45:40 -0500

    minor tweak to tools/winlibs.R
    also add ChangeLog entry and rolls Version: again

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,9 +1,9 @@
 # Build against libprotobuf libraries that were compiled with the Rtools gcc toolchain
 # by Jeroen Ooms. Last updated: December 2014.
-if(!file.exists("../windows/protobuf-2.6.1/include/google/protobuf/descriptor.h")){
-  if(getRversion() <= "3.2.2") setInternet2()
-  download.file("https://github.com/rwinlib/protobuf/archive/v2.6.1.zip", "lib.zip", quiet = TRUE)
-  dir.create("../windows", showWarnings = FALSE)
-  unzip("lib.zip", exdir = "../windows")
-  unlink("lib.zip")
+if (!file.exists("../windows/protobuf-2.6.1/include/google/protobuf/descriptor.h")) {
+    if (getRversion() < "3.3.0") setInternet2()
+    download.file("https://github.com/rwinlib/protobuf/archive/v2.6.1.zip", "lib.zip", quiet = TRUE)
+    dir.create("../windows", showWarnings = FALSE)
+    unzip("lib.zip", exdir = "../windows")
+    unlink("lib.zip")
 }
