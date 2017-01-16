e090300da1e9cb56a586005eb2a5560489bb839e
Dirk Eddelbuettel <edd@debian.org>
2017-01-16 09:08:06 -0600

    use proto3 library on Windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,8 +1,8 @@
-# Build against libprotobuf libraries that were compiled with the Rtools gcc toolchain
-# by Jeroen Ooms. Last updated: December 2014.
-if (!file.exists("../windows/protobuf-2.6.1/include/google/protobuf/descriptor.h")) {
+# Build against protobuf libs compiled with Rtools
+# by Jeroen Ooms. Last updated: August 2016
+if (!file.exists("../windows/protobuf-3.0.0/include/google/protobuf/descriptor.h")) {
     if (getRversion() < "3.3.0") setInternet2()
-    download.file("https://github.com/rwinlib/protobuf/archive/v2.6.1.zip", "lib.zip", quiet = TRUE)
+    download.file("https://github.com/rwinlib/protobuf/archive/v3.0.0.zip", "lib.zip", quiet = TRUE)
     dir.create("../windows", showWarnings = FALSE)
     unzip("lib.zip", exdir = "../windows")
     unlink("lib.zip")
