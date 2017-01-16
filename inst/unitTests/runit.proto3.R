9d5d034ab83e2ae7c19111747644e6581a77edf9
Dirk Eddelbuettel <edd@debian.org>
2017-01-16 10:51:17 -0600

    new unit test

---
+++ inst/unitTests/runit.proto3.R
@@ -0,0 +1,32 @@
+## rprotobuf.h: R/C++ interface class library
+##
+## Copyright (C) 2017 - 2013  Dirk Eddelbuettel
+##
+## This file is part of RProtoBuf.
+##
+## RProtoBuf is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## RProtoBuf is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with RProtoBuf.  If not, see <http://www.gnu.org/licenses/>.
+
+.setUp <- function(){
+    if( !exists("SearchRequest", "RProtoBuf:DescriptorPool")) {
+        unitest.proto.file <- system.file("unitTests", "data", "proto3.proto", package="RProtoBuf")
+        readProtoFiles(file = unitest.proto.file)
+    }
+}
+
+test.proto3 <- function() {
+    q <- new(SearchRequest, query="abc", page_number=42L, result_per_page=77L)
+    checkEquals(q$query, "abc", msg="proto3 string")
+    checkEquals(q$page_number, 42L, msg="proto3 int")
+    checkEquals(q$result_per_page, 77L, msg="proto3 int again")
+}
