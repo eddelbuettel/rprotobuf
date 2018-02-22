40bc4294195eff1f10d5eb4f0a4ef1b3a9c0b7ba
maciejlach <maciej.lach@gmail.com>
2018-02-22 08:18:23 +0000

    [windows-build] upgrade protobuf library to 3.2.0 provided by [rwinlib/protobuf](https://github.com/rwinlib/protobuf/releases/tag/v3.2.0)

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,8 +1,8 @@
 # Build against protobuf libs compiled with Rtools
 # by Jeroen Ooms. Last updated: August 2016
-if (!file.exists("../windows/protobuf-3.0.0/include/google/protobuf/descriptor.h")) {
+if (!file.exists("../windows/protobuf-3.2.0/include/google/protobuf/descriptor.h")) {
     if (getRversion() < "3.3.0") setInternet2()
-    download.file("https://github.com/rwinlib/protobuf/archive/v3.0.0.zip", "lib.zip", quiet = TRUE)
+    download.file("https://github.com/rwinlib/protobuf/archive/v3.2.0.zip", "lib.zip", quiet = TRUE)
     dir.create("../windows", showWarnings = FALSE)
     unzip("lib.zip", exdir = "../windows")
     unlink("lib.zip")
