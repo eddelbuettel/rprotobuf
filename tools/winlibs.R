65815e2cdc4b367bc35b1d15eaa6018d0fc0c99d
Jeroen Ooms <jeroenooms@gmail.com>
2019-06-27 21:53:19 +0200

    Windows: update to protobuf 3.8.0, add support for GCC 8+.

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,8 +1,8 @@
 # Build against protobuf libs compiled with Rtools
-# by Jeroen Ooms. Last updated: June 2018 (for gcc 8.1.0)
-if (!file.exists("../windows/protobuf-3.5.1/include/google/protobuf/descriptor.h")) {
+VERSION <- commandArgs(TRUE)
+if (!file.exists(sprintf("../windows/protobuf-%s/include/google/protobuf/descriptor.h", VERSION))) {
     if (getRversion() < "3.3.0") setInternet2()
-    download.file("https://github.com/rwinlib/protobuf/archive/v3.5.1.zip", "lib.zip", quiet = TRUE)
+    download.file(sprintf("https://github.com/rwinlib/protobuf/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
     dir.create("../windows", showWarnings = FALSE)
     unzip("lib.zip", exdir = "../windows")
     unlink("lib.zip")
