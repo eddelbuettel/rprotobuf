369b9336fdd379230e02eb89d9ff766a22272a6d
Michael Chirico <chiricom@google.com>
2022-04-04 22:59:38 +0000

    adding file

---
+++ R/text_format.R
@@ -0,0 +1,7 @@
+setGeneric( "toTextFormat", function( x ) {
+    standardGeneric( "toTextFormat" )
+} )
+setMethod( "toTextFormat", c( x = "Message"),
+          function(x) {
+              .Call( "Message__print_text_format", x@pointer, PACKAGE = "RProtoBuf")
+          } )
