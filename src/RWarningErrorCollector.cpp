9820bf24bdba974b7cd944f1e0b15d9c0ecb592c
Matteo Gianella <gianeteo@hotmail.it>
2024-07-02 20:47:01 +0200

    `RProtoBuf` compatible with `protobuf` >= 26.x (#104)
    * RProtoBuf compatible with protobuf >= 5.26.0
    
    * update ChangeLog

--- src/RWarningErrorCollector.cpp
+++ src/RWarningErrorCollector.cpp
@@ -4,10 +4,18 @@
 
 namespace rprotobuf {
 
-void RWarningErrorCollector::AddError(const std::string& filename, int line, int column,
-                                      const std::string& message) {
+#if GOOGLE_PROTOBUF_VERSION < 5026000
+    void RWarningErrorCollector::AddError(const std::string& filename, int line, int column,
+                                          const std::string& message) {
 
-    Rprintf("%s:%d:%d:%s\n", filename.c_str(), line + 1, column + 1, message.c_str());
-}
+        Rprintf("%s:%d:%d:%s\n", filename.c_str(), line + 1, column + 1, message.c_str());
+    }
+#else
+    void RWarningErrorCollector::RecordError(absl::string_view filename, int line, int column,
+                                             absl::string_view message) {
+
+        Rprintf("%s:%d:%d:%s\n", std::string(filename).c_str(), line + 1, column + 1, std::string(message).c_str());
+    }
+#endif
 
 }  // namespace rprotobuf
